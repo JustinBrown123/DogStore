a41c504baa732cf30dda6e4a727dc5bd0b694baf
Justin Brown <j-brown123@live.com>
2020-04-01 15:00:24 -0400

    Added Pages

--- OnlineStore/Models/Home/HomeIndexViewModel.cs
+++ OnlineStore/Models/Home/HomeIndexViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using OnlineStore.DAL;
 using OnlineStore.Repository;
+using PagedList;
+using PagedList.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -13,14 +15,14 @@ namespace OnlineStore.Models.Home
     {
         public  GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
         dbMyOnlineShoppingEntities context = new dbMyOnlineShoppingEntities();
-        public IEnumerable<Tbl_Product> ListOfProducts { get; set; }
-        public HomeIndexViewModel CreateModel(string search)
+        public IPagedList<Tbl_Product> ListOfProducts { get; set; }
+        public HomeIndexViewModel CreateModel(string search, int pageSize, int? page)
         {
             SqlParameter[] param = new SqlParameter[]
             {
                 new SqlParameter("@search", search??(object)DBNull.Value)
             };
-            IEnumerable<Tbl_Product> data = context.Database.SqlQuery<Tbl_Product>("GetBySearch @search", param).ToList();
+            IPagedList<Tbl_Product> data = context.Database.SqlQuery<Tbl_Product>("GetBySearch @search", param).ToList().ToPagedList(page ?? 1, pageSize);
             return new HomeIndexViewModel
             {
                 ListOfProducts = data
