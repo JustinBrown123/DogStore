1b7ab2722e7b6fe53965b2d7a9fcf6c27a62e674
Justin Brown <j-brown123@live.com>
2020-04-02 10:29:05 -0400

    working on cart

--- OnlineStore/Controllers/HomeController.cs
+++ OnlineStore/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ namespace OnlineStore.Controllers
 {
     public class HomeController : Controller
     {
+        dbMyOnlineShoppingEntities ctx = new dbMyOnlineShoppingEntities();
         public ActionResult Index(string search, int? page)
         {
             HomeIndexViewModel model = new HomeIndexViewModel();
@@ -31,6 +32,66 @@ namespace OnlineStore.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult AddToCart(int productId)
+        {
+            if (Session["cart"] == null)
+            {
+                List<Item> cart = new List<Item>();
+                var product = ctx.Tbl_Product.Find(productId);
+                cart.Add(new Item()
+                {
+                    Product = product,
+                    Quantity = 1
+                });
+                Session["cart"] = cart;
+
+            }
+            else
+            {
+                List<Item> cart = (List<Item>)Session["cart"];
+                var product = ctx.Tbl_Product.Find(productId);
+                foreach(var item in cart)
+                {
+                    if (item.Product.ProductId == productId)
+                    {
+                        int prevQty = item.Quantity;
+                        cart.Remove(item);
+                        cart.Add(new Item()
+                        {
+                            Product = product,
+                            Quantity = prevQty + 1
+                        });
+                        break;
+                    }
+                    else
+                    {
+                        cart.Add(new Item()
+                        {
+                            Product = product,
+                            Quantity = 1
+                        });
+                    }                   
+                }
+                    Session["cart"] = cart;
+                }
+
+            return Redirect("Index");
+        }
+        public ActionResult RemoveFromCart(int productId)
+        {
+            List<Item> cart = (List<Item>)Session["cart"];
+            foreach (var item in cart)
+            {
+                if(item.Product.ProductId == productId)
+                {
+                    cart.Remove(item);
+                    break;
+                }
+            }
+            Session["cart"] = cart;
+            return Redirect("Index");
+
         }
     }
 }
---
+++ OnlineStore/Models/Home/Item.cs
@@ -0,0 +1,14 @@
+﻿using OnlineStore.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineStore.Models.Home
+{
+    public class Item
+    {
+        public Tbl_Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
